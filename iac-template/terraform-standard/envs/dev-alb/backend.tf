a3f054e8e878191a3ba407b1726a3be751caf677
Haitao Pan <manbuzhe2009@qq.com>
2025-11-17 17:18:21 +0800

    feat(alb): add Application Load Balancer module and dev-alb environment

---
+++ iac-template/terraform-standard/envs/dev-alb/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "svc-plus-iac-state"
+    key            = "account/dev/alb/terraform.tfstate"
+    region         = "ap-northeast-1"
+    dynamodb_table = "svc-plus-iac-state-dynamodb-lock"
+  }
+}
